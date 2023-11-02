d6aca2bd452e423507c2f212deb5aa4d11e29bb6
Javier Poremski <javier@oderland.se>
2023-11-02 16:58:09 +0100

    add lualine status line
    Signed-off-by: Javier Poremski <javier@oderland.se>

--- init.lua
+++ init.lua
@@ -1,7 +1,18 @@
 vim.g.mapleader = ' '
 
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
 
+vim.o.number = true
 vim.o.relativenumber = true
+vim.o.mouse = 'a'
+vim.o.ignorecase = true 
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.wrap = true
+vim.o.breakindent = true
+vim.o.tapstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 require('poremski.packer')
